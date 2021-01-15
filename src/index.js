b36eb182db973537dbfd2ac0c4f5202b0d696115
gorr2012 <sergey_gornost@mail.ru>
2021-01-15 20:12:46 +0300

    feat: implement functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+  array.sort(function(a, b) { return a - b; });
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+  array.sort(function(a, b) { return a - b; });
+  return array[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if(!array || array.length === 0) return 0;
+  let res = 0;
+	for (let i = 0; i < array.length; i++) {
+		res += Number(array[i]);
+	}
+  return Math.round(res/(array.length)*100)/100;
+} 
