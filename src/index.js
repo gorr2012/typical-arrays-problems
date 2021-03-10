f8bbe203dbda28bdf578a1408a78c44fbd3cda0d
gorr2012 <sergey_gornost@mail.ru>
2021-03-10 10:32:18 +0300

    fix: update functions

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 exports.min = function min (array) {
   if(!array || array.length === 0) return 0;
-  array.sort(function(a, b) { return a - b; });
+  array.sort((a, b) => { return a - b });
   return array[0];
 }
 
 exports.max = function max (array) {
   if(!array || array.length === 0) return 0;
-  array.sort(function(a, b) { return a - b; });
+  array.sort((a, b) => { return a - b });
   return array[array.length-1];
 }
 
 exports.avg = function avg (array) {
   if(!array || array.length === 0) return 0;
-  let res = 0;
-	for (let i = 0; i < array.length; i++) {
-		res += Number(array[i]);
-	}
+  let res = array.reduce((sum, cur) => {return sum + cur});
   return Math.round(res/(array.length)*100)/100;
 } 
