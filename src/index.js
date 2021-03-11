4467a58ced848b0580d6d6f1b84fa4fba9ef9822
gorr2012 <sergey_gornost@mail.ru>
2021-03-11 15:40:09 +0300

    refactor: make more shorter solution

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 exports.min = function min (array) {
-  if(!array || array.length === 0) return 0;
-  array.sort((a, b) => { return a - b });
-  return array[0];
+  return (!array || array.length === 0) ? 0 : array.sort((a, b) => { return a - b })[0];
 }
 
 exports.max = function max (array) {
-  if(!array || array.length === 0) return 0;
-  array.sort((a, b) => { return a - b });
-  return array[array.length-1];
+  return (!array || array.length === 0) ? 0 : array.sort((a, b) => { return a - b })[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  if(!array || array.length === 0) return 0;
-  let res = array.reduce((sum, cur) => {return sum + cur});
-  return Math.round(res/(array.length)*100)/100;
+  return (!array || array.length === 0) ? 0 : Math.round(array.reduce((sum, cur) => {return sum + cur})/(array.length)*100)/100;
 } 
